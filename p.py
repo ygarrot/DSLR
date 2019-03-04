5d3a37afae336c92a78034b0236893d09bb8f103
yoan garrot <garrot.yoan1@gmail.com>
2019-03-04 15:09:02 +0100

    remove some hardcoded values

---
+++ p.py
@@ -0,0 +1,4 @@
+import seaborn as sns
+sns.set(style="ticks", color_codes=True)
+iris = sns.load_dataset("iris")
+g = sns.pairplot(iris, hue="species")
--- pair_plot.py
+++ pair_plot.py
@@ -1,13 +1,21 @@
 import matplotlib.pyplot as plt
 from feature_scaling import *
+import seaborn as sns
 import numpy as np
 import pandas as pd
 
 def pair_plot(file):
+    sns.set(style="ticks", color_codes=True)
+
     points = pd.read_csv(file).head(50)
+    points = points.dropna()
+    house_column = 'Hogwarts House'
+    
     no_int = points.select_dtypes(include=['object'])
     only_int = points.select_dtypes(exclude=['object'])
     min_c = only_int.apply(mean_normalization)
-    pd.scatter_matrix(only_int)
-    plt.show()
 
+    houses = points.loc[:, house_column]
+    only_int.loc[:, house_column] = houses.loc[:]
+    sns.pairplot(points, hue=house_column, size=2,)
+    plt.show()
